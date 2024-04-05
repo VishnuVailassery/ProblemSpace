cf11805c969e4bcdd96388976c701318cc34c4b7
vishnu v <vishnu.v@trubridge.com>
2024-04-05 22:37:35 +0530

    IsPowerOfThree

---
+++ ProblemSolver/ProblemSolver/Problems/IsPowerOfThree.cs
@@ -0,0 +1,26 @@
+﻿namespace ProblemSolver.Problems
+{
+    internal static partial class Solution
+    {
+        //https://leetcode.com/problems/power-of-three/
+        public static bool IsPowerOfThree(int n)
+        {
+            if (n == 0)
+            {
+                return false;
+            }
+
+            while (n != 1)
+            {
+                if (n % 3 != 0)
+                {
+                    return false;
+                }
+
+                n = n / 3;
+            }
+
+            return true;
+        }
+    }
+}

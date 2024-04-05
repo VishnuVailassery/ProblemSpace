45f73d2186d8f8808bc94d033606f20831ee8b5d
vishnu v <vishnu.v@trubridge.com>
2024-04-05 21:24:18 +0530

    Create Console app and first easy problem solved

---
+++ ProblemSolver/ProblemSolver/Problems/Problem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolver.Problems
+{
+    internal partial class Problem
+    {
+        //https://leetcode.com/problems/move-zeroes/description/
+        public static void MoveZeroes(int[] nums)
+        {
+            var zCount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zCount++;
+                }
+                else
+                {
+                    nums[i - zCount] = nums[i];
+                }
+            }
+
+            for (int j = 0; j < zCount; j++)
+            {
+                nums[nums.Length - 1 - j] = 0;
+            }
+        }
+    }
+}
